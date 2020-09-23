7971467957c3d2f53197d62f892600cd2dc09457
PjrCodes <pranjal_j_rastogi@yahoo.in>
2020-09-23 23:14:13 +0530

    added file structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # The-elite-ra-project
+
+**Python Version:**
+3.7.x or 3.8.x
---
+++ src/main.py
@@ -0,0 +1,3 @@
+# Copyright (c) 2020 Ayush Gupta, Kartikey Pandey, Pranjal Rastogi
+# main.py
+# entry point for the application
