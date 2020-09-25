28f4afea2456b773339bfc3cfedd0edc429e7aa0
PjrCodes <pranjal_j_rastogi@yahoo.in>
2020-09-25 16:09:02 +0530

    basic integration complete

--- src/levels/mine.py
+++ src/levels/mine.py
@@ -1,40 +1,31 @@
+# written by Ayush Gupta
+
 import pygame
 import random
-from pathlib import Path
 import time
 import platform
-
-pygame.init()
-
-clock = pygame.time.Clock()
+import constants as consts
+from utils import colors
 
 game_stat = 'win'
 
-screen = pygame.display.set_mode((800, 600))
-
-background_colour = (0, 255, 255)
-
-ROOT_PATH = str(Path(__file__).parents[2])
-
-pygame.display.set_caption('Mine')
-
 if platform.system() == "Windows":
-    playerImg = pygame.image.load(fr'{ROOT_PATH}\assets\sprites\test_Drill.png')
-    digImg = pygame.image.load(fr'{ROOT_PATH}\assets\images\textures\digmark.png')
-    crystal = pygame.image.load(fr'{ROOT_PATH}\assets\images\textures\crystal.png')
-    buttonImg = pygame.image.load(fr'{ROOT_PATH}\assets\images\textures\UI\button.png')
-    single_meteor_img = pygame.image.load(fr'{ROOT_PATH}\assets\images\textures\nuclear.png')
-    single_rock_img = pygame.image.load(fr'{ROOT_PATH}\assets\images\textures\stone.png')
-    single_oil_img = pygame.image.load(fr'{ROOT_PATH}\assets\images\textures\oil.png')
+    playerImg = pygame.image.load(fr'{consts.ROOT_PATH}\assets\sprites\test_Drill.png')
+    digImg = pygame.image.load(fr'{consts.ROOT_PATH}\assets\images\textures\digmark.png')
+    crystal = pygame.image.load(fr'{consts.ROOT_PATH}\assets\images\textures\crystal.png')
+    buttonImg = pygame.image.load(fr'{consts.ROOT_PATH}\assets\images\textures\UI\button.png')
+    single_meteor_img = pygame.image.load(fr'{consts.ROOT_PATH}\assets\images\textures\nuclear.png')
+    single_rock_img = pygame.image.load(fr'{consts.ROOT_PATH}\assets\images\textures\stone.png')
+    single_oil_img = pygame.image.load(fr'{consts.ROOT_PATH}\assets\images\textures\oil.png')
 
 else:
-    playerImg = pygame.image.load(fr'{ROOT_PATH}/assets/sprites/test_Drill.png')
-    digImg = pygame.image.load(fr'{ROOT_PATH}/assets/images/textures/digmark.png')
-    crystal = pygame.image.load(fr'{ROOT_PATH}/assets/images/textures/crystal.png')
-    buttonImg = pygame.image.load(fr'{ROOT_PATH}/assets/images/textures/UI/button.png')
-    single_meteor_img = pygame.image.load(fr'{ROOT_PATH}/assets/images/textures/nuclear.png')
-    single_rock_img = pygame.image.load(fr'{ROOT_PATH}/assets/images/textures/stone.png')
-    single_oil_img = pygame.image.load(fr'{ROOT_PATH}/assets/images/textures/oil.png')
+    playerImg = pygame.image.load(fr'{consts.ROOT_PATH}/assets/sprites/test_Drill.png')
+    digImg = pygame.image.load(fr'{consts.ROOT_PATH}/assets/images/textures/digmark.png')
+    crystal = pygame.image.load(fr'{consts.ROOT_PATH}/assets/images/textures/crystal.png')
+    buttonImg = pygame.image.load(fr'{consts.ROOT_PATH}/assets/images/textures/UI/button.png')
+    single_meteor_img = pygame.image.load(fr'{consts.ROOT_PATH}/assets/images/textures/nuclear.png')
+    single_rock_img = pygame.image.load(fr'{consts.ROOT_PATH}/assets/images/textures/stone.png')
+    single_oil_img = pygame.image.load(fr'{consts.ROOT_PATH}/assets/images/textures/oil.png')
 
 time1 = 0
 
@@ -78,8 +69,6 @@ for i in range(num_of_oils):
     oilY.append(random.randint(600, 1200))
     oilY_change.append(-3)
 
-over_font = pygame.font.Font('freesansbold.ttf', 64)
-timer = pygame.font.Font('freesansbold.ttf', 12)
 
 digX = [playerX]
 digY = [i for i in range(playerY - 31, -64, -3)]
@@ -101,173 +90,172 @@ game_stat1 = 'lost'
 
 
 def player(x, y):
-    screen.blit(playerImg, (x, y))
+    consts.MAIN_DISPLAY.blit(playerImg, (x, y))
 
 
 def meteor(x, y, _):
-    screen.blit(meteorImg[_], (x, y))
+    consts.MAIN_DISPLAY.blit(meteorImg[_], (x, y))
 
 
 def rock(x, y, _):
-    screen.blit(rockImg[_], (x, y))
+    consts.MAIN_DISPLAY.blit(rockImg[_], (x, y))
 
 
 def oil(x, y, _):
-    screen.blit(oilImg[_], (x, y))
+    consts.MAIN_DISPLAY.blit(oilImg[_], (x, y))
 
 
 def dig(x, y, _):
-    screen.blit(digImg, (x, y))
+    consts.MAIN_DISPLAY.blit(digImg, (x, y))
 
 
 def display_button():
-    screen.blit(buttonImg, (368, 268))
+    consts.MAIN_DISPLAY.blit(buttonImg, (368, 268))
 
 
 def show_crystal(x, y):
-    screen.blit(crystal, (x, y))
+    consts.MAIN_DISPLAY.blit(crystal, (x, y))
 
 
 def game_over_text(text, x, y):
-    gameover = over_font.render(text, True, (255, 255, 255))
-    screen.blit(gameover, (x, y))
+    gameover = consts.TITLE_FONT.render(text, True, colors.WHITE_COLOR)
+    consts.MAIN_DISPLAY.blit(gameover, (x, y))
 
 
 def display_time(timern):
-    time_display = timer.render(timern, True, (255, 255, 255))
-    screen.blit(time_display, (770, 10))
+    time_display = consts.BUTTON_FONT.render(timern, True, colors.WHITE_COLOR)
+    consts.MAIN_DISPLAY.blit(time_display, (770, 10))
 
 
-running = True
+def mine_level():
+    running = True
+    global playerX_change, button_stat, time1, game_stat1, game_stat, playerY, playerX, digX, digY
+    while running:
 
-while running:
+        # TODO: replace with colors.BROWN
+        consts.MAIN_DISPLAY.fill((150, 80, 30))
 
-    screen.fill((150, 80, 30))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -3
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -3
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 3
 
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 3
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0
-
-    if button_stat == 'free' or button_stat == 'free1':
-        display_button()
-        if pygame.mouse.get_pressed()[0]:
-            a = pygame.mouse.get_pos()[0]
-            b = pygame.mouse.get_pos()[1]
-            if button.collidepoint(a, b):
-                time1 = time.time()
-                button_stat = 'pressed'
-
-    else:
-        if game_stat == 'win':
-            time_taken = round(time.time() - time1, 1)
-            display_time(str(time_taken))
-            if time_taken < 30:
-                for i in range(num_of_meteors):
-
-                    collision = is_collision(meteorX[i], meteorY[i], playerX, playerY)
-
-                    if collision:
-                        playerY = 2000
-                        game_stat = 'lost'
-
-                    meteorY[i] += meteorY_change[i]
-
-                    meteor(meteorX[i], meteorY[i], i)
-
-                    if meteorY[i] < -32:
-                        meteorX[i] = random.randint(0, 736)
-                        meteorY[i] = random.randint(600, 1200)
-
-                for i in range(num_of_rocks):
-
-                    collision = is_collision(rockX[i], rockY[i], playerX, playerY)
-
-                    if collision:
-                        playerY = 2000
-                        game_stat = 'lost'
-
-                    rockY[i] += rockY_change[i]
-
-                    rock(rockX[i], rockY[i], i)
-
-                    if rockY[i] < -64:
-                        rockX[i] = random.randint(0, 736)
-                        rockY[i] = random.randint(600, 1200)
-
-                for i in range(num_of_oils):
-
-                    collision = is_collision(oilX[i], oilY[i], playerX, playerY)
-
-                    if collision:
-                        playerY = 2000
-                        game_stat = 'lost'
-
-                    oilY[i] += oilY_change[i]
-
-                    oil(oilX[i], oilY[i], i)
-
-                    if oilY[i] < -32:
-                        oilX[i] = random.randint(0, 736)
-                        oilY[i] = random.randint(600, 1200)
-            else:
-                if game_stat1 == 'lost':
-                    count = 0
-                game_stat1 = 'win'
-
-            if playerX > 736:
-                playerX = 736
-            elif playerX <= 0:
-                playerX = 0
-
-        if game_stat == 'lost':
-            game_over_text('GAME OVER', 200, 250)
+        if button_stat == 'free' or button_stat == 'free1':
             display_button()
             if pygame.mouse.get_pressed()[0]:
                 a = pygame.mouse.get_pos()[0]
                 b = pygame.mouse.get_pos()[1]
                 if button.collidepoint(a, b):
                     time1 = time.time()
+                    button_stat = 'pressed'
+
+        else:
+            if game_stat == 'win':
+                time_taken = round(time.time() - time1, 1)
+                display_time(str(time_taken))
+                if time_taken < 30:
                     for i in range(num_of_meteors):
-                        meteorX[i] = random.randint(0, 736)
-                        meteorY[i] = random.randint(600, 1200)
+                        collision = is_collision(meteorX[i], meteorY[i], playerX, playerY)
+
+                        if collision:
+                            playerY = 2000
+                            game_stat = 'lost'
+
+                        meteorY[i] += meteorY_change[i]
+
+                        meteor(meteorX[i], meteorY[i], i)
+
+                        if meteorY[i] < -32:
+                            meteorX[i] = random.randint(0, 736)
+                            meteorY[i] = random.randint(600, 1200)
+
                     for i in range(num_of_rocks):
-                        rockX[i] = random.randint(0, 736)
-                        rockY[i] = random.randint(600, 1200)
+                        collision = is_collision(rockX[i], rockY[i], playerX, playerY)
+
+                        if collision:
+                            playerY = 2000
+                            game_stat = 'lost'
+
+                        rockY[i] += rockY_change[i]
+
+                        rock(rockX[i], rockY[i], i)
+
+                        if rockY[i] < -64:
+                            rockX[i] = random.randint(0, 736)
+                            rockY[i] = random.randint(600, 1200)
 
                     for i in range(num_of_oils):
-                        oilX[i] = random.randint(0, 736)
-                        oilY[i] = random.randint(600, 1200)
-                    button_stat = 'pressed'
-                    game_stat = 'win'
-                    game_stat1 = 'lost'
-                    playerY = 150
-        playerX += playerX_change
-        digX.insert(0, playerX)
+                        collision = is_collision(oilX[i], oilY[i], playerX, playerY)
 
-        digX = digX[:len(digY)]
+                        if collision:
+                            playerY = 2000
+                            game_stat = 'lost'
 
-        for i in range(len(digX)):
-            dig(digX[i], digY[i], i)
+                        oilY[i] += oilY_change[i]
 
-        if game_stat1 == 'win':
-            count += 3
-            pygame.draw.line(screen, (255, 255, 255), (0, 600 - count),
-                             (800, 600 - count), 5)
-            show_crystal(368, 640 - count)
-            if 600 - count < 150:
-                game_over_text('YOU GOT THE CRYSTAL', 10, 250)
-                # END CODE HERE
-        player(playerX, playerY)
-    pygame.display.update()
-    clock.tick(60)
+                        oil(oilX[i], oilY[i], i)
+
+                        if oilY[i] < -32:
+                            oilX[i] = random.randint(0, 736)
+                            oilY[i] = random.randint(600, 1200)
+                else:
+                    if game_stat1 == 'lost':
+                        count = 0
+                    game_stat1 = 'win'
+
+                if playerX > 736:
+                    playerX = 736
+                elif playerX <= 0:
+                    playerX = 0
+
+            if game_stat == 'lost':
+                game_over_text('GAME OVER', 200, 250)
+                display_button()
+                if pygame.mouse.get_pressed()[0]:
+                    a = pygame.mouse.get_pos()[0]
+                    b = pygame.mouse.get_pos()[1]
+                    if button.collidepoint(a, b):
+                        time1 = time.time()
+                        for i in range(num_of_meteors):
+                            meteorX[i] = random.randint(0, 736)
+                            meteorY[i] = random.randint(600, 1200)
+                        for i in range(num_of_rocks):
+                            rockX[i] = random.randint(0, 736)
+                            rockY[i] = random.randint(600, 1200)
+
+                        for i in range(num_of_oils):
+                            oilX[i] = random.randint(0, 736)
+                            oilY[i] = random.randint(600, 1200)
+                        button_stat = 'pressed'
+                        game_stat = 'win'
+                        game_stat1 = 'lost'
+                        playerY = 150
+            playerX += playerX_change
+            digX.insert(0, playerX)
+
+            digX = digX[:len(digY)]
+
+            for i in range(len(digX)):
+                dig(digX[i], digY[i], i)
+
+            if game_stat1 == 'win':
+                count += 3
+                pygame.draw.line(consts.MAIN_DISPLAY, (255, 255, 255), (0, 600 - count),
+                                 (800, 600 - count), 5)
+                show_crystal(368, 640 - count)
+                if 600 - count < 150:
+                    game_over_text('YOU GOT THE CRYSTAL', 10, 250)
+                    # END CODE HERE
+            player(playerX, playerY)
+        pygame.display.update()
+        consts.CLOCK.tick(60)
--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ import sys
 from utils import colors
 from utils import widgets
 import constants as consts
+from levels import mine
 
 
 # init window and clock
@@ -17,27 +18,22 @@ import constants as consts
 
 pygame.display.set_caption(consts.SCREEN_TITLE)
 # TODO: Bg image
-# TITLE_SCREEN_BACKGROUND_IMAGE = pygame.image.load(f"{consts.ROOT_PATH}/assets/images/bg/bg.png").convert()
-# consts.MAIN_DISPLAY.blit(TITLE_SCREEN_BACKGROUND_IMAGE, [0, 0])
-consts.MAIN_DISPLAY.fill(colors.BLACK_COLOR) # TODO: remove
+TITLE_SCREEN_BACKGROUND_IMAGE = pygame.image.load(f"{consts.ROOT_PATH}/assets/images/bg/bg.png").convert()
+consts.MAIN_DISPLAY.blit(TITLE_SCREEN_BACKGROUND_IMAGE, [0, 0])
 
 # TODO: Load game settings from another file
 
-# this can be static, doesnt need to be remade each time game is run, but, it is remade...
-title = consts.TITLE_FONT.render(consts.SCREEN_TITLE, True, colors.THEME_ALT_LIGHT)
-
 
 def starting_screen():
+
+    play_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=((consts.SCREEN_WIDTH / 2) - 100,
+                                                                       (consts.SCREEN_HEIGHT / 2) - 20),
+                                     width=200, height=40, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
+                                     font=consts.BOLD_FONT, text='Play')
     # the main game loop, looped every frame, looped every clock.tick(TICK_RATE)
     is_game_over = False
-    mouse_down = False
     while not is_game_over:
-
-        play_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=((consts.SCREEN_WIDTH / 2) - 100,
-                                                                           (consts.SCREEN_HEIGHT / 2) - 20),
-                                         width=200, height=40, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
-                                         font=consts.BOLD_FONT, text='Play')
-
+        mouse_down = False
         # gets all the events occurring every frame, which can be mouse movement, mouse click, etc.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -62,20 +58,54 @@ def starting_screen():
 
 def level_chooser_screen():
     pygame.display.set_caption(consts.SCREEN_TITLE)
-    consts.MAIN_DISPLAY.fill(colors.BLACK_COLOR)  # TODO: replace with image!
-    # TODO: blit image for LEVELS TITLE
+    consts.MAIN_DISPLAY.blit(TITLE_SCREEN_BACKGROUND_IMAGE, [0, 0])
+    # Render font
+    levels_title = consts.TITLE_FONT.render("LEVELS", True, colors.WHITE_COLOR)
+    consts.MAIN_DISPLAY.blit(levels_title, (40, 10))
+
+    pygame.draw.line(consts.MAIN_DISPLAY, colors.WHITE_COLOR, (5, 10 + levels_title.get_height()),
+                     (consts.SCREEN_WIDTH-10, 10 + levels_title.get_height()), 3)
 
     # TODO: blit buttons for levels 1 thru 6
+    lvl1_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=(10, levels_title.get_height() + 30),
+                                     width=50, height=50, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
+                                     font=consts.BOLD_FONT, text='1')
+    lvl2_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=(70, levels_title.get_height() + 30),
+                                     width=50, height=50, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
+                                     font=consts.BOLD_FONT, text='2')
+    lvl3_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=(130, levels_title.get_height() + 30),
+                                     width=50, height=50, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
+                                     font=consts.BOLD_FONT, text='3')
+    lvl4_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=(190, levels_title.get_height() + 30),
+                                     width=50, height=50, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
+                                     font=consts.BOLD_FONT, text='4')
+    lvl5_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=(250, levels_title.get_height() + 30),
+                                     width=50, height=50, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
+                                     font=consts.BOLD_FONT, text='5')
+    lvl6_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=(310, levels_title.get_height() + 30),
+                                     width=50, height=50, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
+                                     font=consts.BOLD_FONT, text='6')
 
     is_game_over = False
     while not is_game_over:
+        mouse_down = False
         # gets all the events occurring every frame, which can be mouse movement, mouse click, etc.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 # quit game if QUIT is invoked
                 is_game_over = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_down = True
 
         # TODO: add button handlers
+        if lvl1_button.hovered:
+            lvl1_button.toggle_bg(colors.THEME_ALT_DARK)
+            if mouse_down:
+                lvl1_button.toggle_bg(colors.THEME_ALT)
+                print("SCAM")
+                return mine.mine_level()
+        else:
+            lvl1_button.toggle_bg(colors.THEME_ALT)
 
         # update all the things in game
         pygame.display.update()
