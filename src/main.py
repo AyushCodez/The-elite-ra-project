c27625f240c00ff5e7a5043ea6a69693331ee40e
Kartikey_Pandey1884 <51755548+kartikey-onlineGOD@users.noreply.github.com>
2020-09-26 18:24:27 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -32,8 +32,8 @@ class Logo(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
         self.image = pygame.Surface([width, height])
-        self.image.fill(WHITE)
-        self.image.set_colorkey(WHITE)
+        self.image.fill(WHITE_COLOR)
+        self.image.set_colorkey(WHITE_COLOR)
         self.image = pygame.image.load("assets/images/LOGO.jpg").convert_alpha()
         self.rect = self.image.get_rect()
 
