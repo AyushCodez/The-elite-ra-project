f78d0b6cbf86da86c768997a49263e13f76acbac
Kartikey_Pandey1884 <51755548+kartikey-onlineGOD@users.noreply.github.com>
2020-09-26 16:40:52 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -44,6 +44,10 @@ def starting_screen():
                                                                        (consts.SCREEN_HEIGHT / 2) - 20),
                                      width=200, height=40, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
                                      font=consts.BOLD_FONT, text='Play')
+    
+    mute_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=(750 , 550),
+                                     width=50, height=20, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
+                                     font=consts.BOLD_FONT, text='mute')
     # the main game loop, looped every frame, looped every clock.tick(TICK_RATE)
     is_game_over = False
     while not is_game_over:
@@ -64,7 +68,9 @@ def starting_screen():
                 return levelscreen.level_chooser_screen()
         else:
             play_button.toggle_bg(colors.THEME_ALT)
-
+        if mute_button.hovered :
+            if event.type == pygame.MOUSEBUTTONDOWN :
+                mixer.music.set_volume(0.0)
         # update all the things in game
         pygame.display.update()
         consts.CLOCK.tick(consts.TICK_RATE)
