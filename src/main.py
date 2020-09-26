2f463417d49e99bf16442446a555615575b92b46
PjrCodes <pranjal_j_rastogi@yahoo.in>
2020-09-26 18:31:54 +0530

    fixed errors

--- src/main.py
+++ src/main.py
@@ -19,51 +19,40 @@ from pygame import mixer
 if platform.system() == 'Windows':
     icon = pygame.image.load(fr'{consts.ROOT_PATH}\assets\images\icon.png')
     bg_music = fr'{consts.ROOT_PATH}\assets\audio\bg\bg_song.mp3'
+    logo_path = fr'{consts.ROOT_PATH}\assets\images\LOGO.jpg'
 
 else:
     icon = pygame.image.load(fr'{consts.ROOT_PATH}/assets/images/icon.png')
     bg_music = fr'{consts.ROOT_PATH}/assets/audio/bg/bg_song.mp3'
+    logo_path = fr'{consts.ROOT_PATH}/assets/images/LOGO.jpg'
 
 mixer.music.set_volume(0.4)
 mixer.music.load(bg_music)
 mixer.music.play(-1)
 
-class Logo(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(WHITE_COLOR)
-        self.image.set_colorkey(WHITE_COLOR)
-        self.image = pygame.image.load("assets/images/LOGO.jpg").convert_alpha()
-        self.rect = self.image.get_rect()
+logo_image = pygame.image.load(logo_path).convert_alpha()
+
 
 # init window and clock
 pygame.display.set_icon(icon)
 
 pygame.display.set_caption(consts.SCREEN_TITLE)
 consts.MAIN_DISPLAY.blit(consts.TITLE_SCREEN_BACKGROUND_IMAGE, [0, 0])
-
+consts.MAIN_DISPLAY.blit(logo_image, (100, 10))
 
 # TODO: Load game settings from another file (optional)
 
 
 def starting_screen():
     play_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=((consts.SCREEN_WIDTH / 2) - 100,
-                                                                       (consts.SCREEN_HEIGHT / 2) - 20),
+                                                                       (consts.SCREEN_HEIGHT / 2) + 100),
                                      width=200, height=40, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
                                      font=consts.BOLD_FONT, text='Play')
     
     mute_button = widgets.TextButton(surface=consts.MAIN_DISPLAY, pos=(750 , 550),
                                      width=50, height=20, fg_color=colors.WHITE_COLOR, bg_color=colors.THEME_ALT,
                                      font=consts.BOLD_FONT, text='mute')
-    
-    all_sprites_list = pygame.sprite.Group()
 
-    AL = Logo((40,0,0), 20, 30)
-    AL.rect.x = 400
-    AL.rect.y = 200
-    all_sprites_list.add(AL)
-    
     # the main game loop, looped every frame, looped every clock.tick(TICK_RATE)
     is_game_over = False
     while not is_game_over:
@@ -84,12 +73,13 @@ def starting_screen():
                 return levelscreen.level_chooser_screen()
         else:
             play_button.toggle_bg(colors.THEME_ALT)
-        if mute_button.hovered :
-            if event.type == pygame.MOUSEBUTTONDOWN :
+
+        if mute_button.hovered:
+
+            if mouse_down:
                 mixer.music.set_volume(0.0)
+
         # update all the things in game
-        all_sprites_list.update()
-        consts.MAIN_DISPLAY.all_sprites_list.draw(screen)
         pygame.display.update()
         consts.CLOCK.tick(consts.TICK_RATE)
 
